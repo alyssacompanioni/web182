43e302d5b24cc9bc3a3cf0e7fef7e5c09f8bb1f4
alyssa <alyssa.companioni@gmail.com>
2025-03-31 18:14:35 -0400

    updates new.php

--- sas/phase04/public/salamanders/new.php
+++ sas/phase04/public/salamanders/new.php
@@ -1,5 +1,25 @@
-<?php require_once('../../private/initialize.php'); 
-include(SHARED_PATH . '/salamander-header.php');
-echo "<h1>Stub for New Salamander</h1>";
+<?php
+require_once('../../private/initialize.php');
 
-include(SHARED_PATH . '/salamander-footer.php'); ?>
+$page_title = 'Create Salamander';
+include(SHARED_PATH . '/salamander-header.php');
+?>
+
+<h1>Create Salamander</h1>
+
+<form action="create.php" method="post">
+  <label for="name">Name: </label><br>
+  <input type="text" id="name" value=""><br><br>
+
+  <label for="habitat">Habitat: </label><br>
+  <textarea rows="4" cols="50" id="habitat" value=""></textarea><br><br>
+
+  <label for="description">Description: </label><br>
+  <textarea rows="4" cols="50" id="habitat" value=""></textarea><br><br>
+
+  <button type="submit">Create Salamander</button>
+</form>
+
+<?php
+include(SHARED_PATH . '/salamander-footer.php');
+?>
